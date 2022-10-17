3c921466f9892079a8e2046149a9c1cf3d4b3243
João Vitor <joaovitorsouza205@hotmail.com>
2022-10-16 23:46:39 -0300

    style: formatação black

--- app.py
+++ app.py
@@ -16,7 +16,10 @@ dataset = pd.read_csv("datalake/curated/dataset.csv")
 # Título
 st.title("Human Resource Analytics")
 
-st.markdown("""<p style="font-family:Cambria; font-size: 20px; text-align: justify">Esse projeto foi feito para resolver um problema comum na área de <i>Human Resources</i> (ou RH em português) chamado de <i>Churn</i>, ou mais espeficamente <i>Churn Rate</i>. O <i>Churn Rate</i> é taxa com a qual um número de pessoas ou objetos saem de um grupo. Aplicando esse conceito ao âmbito empresarial, temos que o <i>Churn Rate</i> se aplica à taxa com que funcionários são demitidos, e a isso está associado todo o custo de a empresa perder alguém com uma função chave, ter que operar com menor capacidade, além de refazer o processo seletivo. Tudo isso despende tempo e dinheiro, e é visando evitar isso que fizemos essa aplicação. <br><br> No menu abaixo você pode escolher alguns atributos associados ao colaborador da empresa, e então o modelo de <i>Machine Learning</i> irá prever a probabilidade desse funcionário pedir demissão. Perceba que se o modelo previr uma alta taxa de evasão, o recomendado será entrar em contato com esse colaborador para melhorarmos a relação entre ele e a empresa, tentando impedir a rotatividade de funcionários. O gráfico abaixo mostra os três principais grupos de empregados que saíram da empresa, os eixos do gráfico são a nota que a empresa deu a eles e quais foram as suas notas de satisfação em relação à empresa antes de deixá-la. O empregado avaliado aparecerá como um ponto amarelo no gráfico. Perceba que estamos considerando outras duas características que não aparecem no gráfico, as horas trabalhadas e o tempo na empresa, então, se o empregado aparecer dentro de algum grupo não necessariamente ele terá chances altas de sair.</p>""", unsafe_allow_html=True)
+st.markdown(
+    """<p style="font-family:Cambria; font-size: 20px; text-align: justify">Esse projeto foi feito para resolver um problema comum na área de <i>Human Resources</i> (ou RH em português) chamado de <i>Churn</i>, ou mais espeficamente <i>Churn Rate</i>. O <i>Churn Rate</i> é taxa com a qual um número de pessoas ou objetos saem de um grupo. Aplicando esse conceito ao âmbito empresarial, temos que o <i>Churn Rate</i> se aplica à taxa com que funcionários são demitidos, e a isso está associado todo o custo de a empresa perder alguém com uma função chave, ter que operar com menor capacidade, além de refazer o processo seletivo. Tudo isso despende tempo e dinheiro, e é visando evitar isso que fizemos essa aplicação. <br><br> No menu abaixo você pode escolher alguns atributos associados ao colaborador da empresa, e então o modelo de <i>Machine Learning</i> irá prever a probabilidade desse funcionário pedir demissão. Perceba que se o modelo previr uma alta taxa de evasão, o recomendado será entrar em contato com esse colaborador para melhorarmos a relação entre ele e a empresa, tentando impedir a rotatividade de funcionários. O gráfico abaixo mostra os três principais grupos de empregados que saíram da empresa, os eixos do gráfico são a nota que a empresa deu a eles e quais foram as suas notas de satisfação em relação à empresa antes de deixá-la. O empregado avaliado aparecerá como um ponto amarelo no gráfico. Perceba que estamos considerando outras duas características que não aparecem no gráfico, as horas trabalhadas e o tempo na empresa, então, se o empregado aparecer dentro de algum grupo não necessariamente ele terá chances altas de sair.</p>""",
+    unsafe_allow_html=True,
+)
 
 "---"
 
@@ -95,11 +98,20 @@ st.plotly_chart(fig)
 
 # Mostrando o valor da predição.
 if classe == 1:
-    st.markdown(f"""<p style="font-family:Cambria; font-size: 23px; text-align: center">Predição do modelo: <b>Evasão</b> com probabilidade de <b>{prob:.2f}%</b></p>""", unsafe_allow_html=True)
+    st.markdown(
+        f"""<p style="font-family:Cambria; font-size: 23px; text-align: center">Predição do modelo: <b>Evasão</b> com probabilidade de <b>{prob:.2f}%</b></p>""",
+        unsafe_allow_html=True,
+    )
 else:
-    st.markdown(f"""<p style="font-family:Cambria; font-size: 23px; text-align: center">Predição do modelo: <b>Permanência</b> com probabilidade de <b>{prob:.2f}%</b></p>""", unsafe_allow_html=True)
+    st.markdown(
+        f"""<p style="font-family:Cambria; font-size: 23px; text-align: center">Predição do modelo: <b>Permanência</b> com probabilidade de <b>{prob:.2f}%</b></p>""",
+        unsafe_allow_html=True,
+    )
 
 "---"
 
 # Logo do Github.
-st.markdown("""<p style="text-align: center;"><a style="text -decoration> none" href="https://github.com/joao-vitor-souza/bootcamp-stack-2021"><img src="https://i.ibb.co/PYvPb4r/imagem-menu.png" alt="Github" width="220" height="82"></a></p>""", unsafe_allow_html=True)
+st.markdown(
+    """<p style="text-align: center;"><a style="text -decoration> none" href="https://github.com/joao-vitor-souza/bootcamp-stack-2021"><img src="https://i.ibb.co/PYvPb4r/imagem-menu.png" alt="Github" width="220" height="82"></a></p>""",
+    unsafe_allow_html=True,
+)
